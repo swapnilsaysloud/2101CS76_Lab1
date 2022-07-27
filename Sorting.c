074d9c04a0a6a5523aa5a39131c8ceed260a764c
Swapnil <swapnilsaraswat101@gmail.com>
2022-07-27 19:36:24 +0600

    Bubble sort done

--- Sorting.c
+++ Sorting.c
@@ -20,6 +20,12 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 void selectionSort(int arr[], int n)
 {
     int i, j, min_idx;
@@ -34,6 +40,16 @@ void selectionSort(int arr[], int n)
         // Swap the found minimum element with the first element
         swap(&arr[min_idx], &arr[i]);
     }
+}
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
 }
 // A function to print an array of size n
 void printArray(int arr[], int n)
@@ -65,6 +81,11 @@ int main()
         break;
     case 2: // For selection sort
         selectionSort(arr, n);
+        printf("Sorted array: \n");
+        printArray(arr, n);
+        break;
+    case 3: // For bubble sort
+        bubbleSort(arr, n);
         printf("Sorted array: \n");
         printArray(arr, n);
         break;
