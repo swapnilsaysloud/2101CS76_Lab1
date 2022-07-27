3d89c49cb3283ae5499328a0d6057de4cc82a01c
Swapnil <swapnilsaraswat101@gmail.com>
2022-07-27 19:18:46 +0600

    Addition part done

---
+++ Calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+
+{
+    char op;
+    double num1, num2;
+    printf("Enter an operator (+, -, *, /):\n ");
+    scanf("%c", &op);
+
+    printf("Enter two numbers for operation:\n ");
+    scanf("%lf %lf", &num1, &num2);
+    switch (op)
+    {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", num1, num2, num1 + num2);
+        break;
+    }
+    return 0;
+}
