aa204c7ed969f8f095679381e2c1cb0f47ae921a
Swapnil <swapnilsaraswat101@gmail.com>
2022-07-27 19:23:38 +0600

    Comments added

--- Calculator.c
+++ Calculator.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
-
+/* C program that works as a calculator */
 {
     char op;
+    /* Numbers on which all the operations will take place*/
     double num1, num2;
     printf("Enter an operator (+, -, *, /):\n ");
     scanf("%c", &op);
-
+    /* Remember the order of entry of numbers for operations like division and subtraction */
     printf("Enter two numbers for operation:\n ");
     scanf("%lf %lf", &num1, &num2);
     switch (op)
@@ -23,6 +24,7 @@ int main()
     case '/':
         printf("%.1lf / %.1lf = %.1lf", num1, num2, num1 / num2);
         break;
+        /* If the input does not match with a valid operator */
     default:
         printf("Error! please use a valid operator \n");
     }
